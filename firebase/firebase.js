12720cf706f60d67bcc579ad5922dccbc864dea3
Trista Zecchel <ta.zecchel@gmail.com>
2026-02-09 14:21:52 -0500

    Created .env file and added the Firebase API key to .env

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -8,7 +8,7 @@ import { getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "flexihand.firebaseapp.com",
   projectId: "flexihand",
   storageBucket: "flexihand.firebasestorage.app",
