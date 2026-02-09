a2da862d50c7180fa50c903abb5f48c7a140fd1d
Trista Zecchel <ta.zecchel@gmail.com>
2026-02-09 09:20:43 -0500

    removed firebase api key

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -8,7 +8,7 @@ import { getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAlp6JRpFaLsCC6uOSDhQi0MBGtXcu1KqI",
+  apiKey: "",
   authDomain: "flexihand.firebaseapp.com",
   projectId: "flexihand",
   storageBucket: "flexihand.firebasestorage.app",
